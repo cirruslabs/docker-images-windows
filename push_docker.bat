3265ed63f52a207986932968b323038b3f67144c
fedor <fedor.korotkov@gmail.com>
2018-05-31 11:14:57 -0400

    push to azure

--- push_docker.bat
+++ push_docker.bat
@@ -4,7 +4,9 @@ IF "%$CIRRUS_BRANCH%" == "master" GOTO NOTMASTER
 :YESPATH
 
 docker login --username "%DOCKER_USER_NAME%" --password "%DOCKER_PASSWORD%"
+docker login cirrusci.azurecr.io --username "%AZURE_USER_NAME%" --password "%AZURE_PASSWORD%"
 docker push cirrusci/windowsservercore:2016
+docker push cirrusci.azurecr.io/cirrusci/windowsservercore:2016
 
 GOTO END
 :NOTMASTER
